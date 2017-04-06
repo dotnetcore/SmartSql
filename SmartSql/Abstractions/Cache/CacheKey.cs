af7588c925b4e9df805208cf63857b6c72b1597d
Ahoo <tsgx_1989@qq.com>
2017-04-06 17:30:26 +0800

    add CacheKey.cs

---
+++ SmartSql/Abstractions/Cache/CacheKey.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSql.Abstractions.Cache
+{
+    public class CacheKey
+    {
+        /// <summary>
+        /// 缓存前缀
+        /// </summary>
+        public String Prefix { get; set; } = "SmartSql-Cache";
+        public IRequestContext RequestContext { get; private set; }
+        public CacheKey(RequestContext context)
+        {
+            RequestContext = context;
+        }
+        public override string ToString()
+        {
+            string key = $"{Prefix}：{RequestContext.FullSqlId}:{RequestContext.Request}";//此处 RequestContext.Request 需重新处理
+            return key;
+        }
+    }
+}
