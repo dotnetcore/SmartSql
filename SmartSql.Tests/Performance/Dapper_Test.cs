58604dc1cc8737088c907de3d505f7d9475d8482
Ahoo <tsgx_1989@qq.com>
2017-04-20 15:06:35 +0800

    u

--- SmartSql.Tests/Performance/Dapper_Test.cs
+++ SmartSql.Tests/Performance/Dapper_Test.cs
@@ -10,14 +10,14 @@ namespace SmartSql.Tests.Performance
 {
     public class Dapper_Test
     {
-        int testTime = 100;
+        int testTime = 100000;
         [Fact]
         public void Query()
         {
             using (var conn = SqlClientFactory.Instance.CreateConnection())
             {
                 conn.ConnectionString = "Data Source=.;database=TestDB;uid=sa;pwd=SmartSql.net";
-                var list = conn.Query<T_Test>("Select Top 10 * From T_Test T Where 1=1  And 2=2 Order By Id Desc   ");
+                var list = conn.Query<T_Test>("SELECT Top 10 T.* From T_Test T With(NoLock) Where 1=1");
             }
         }
 
@@ -30,7 +30,7 @@ namespace SmartSql.Tests.Performance
                 conn.ConnectionString = "Data Source=.;database=TestDB;uid=sa;pwd=SmartSql.net";
                 for (i = 0; i < testTime; i++)
                 {
-                    var list = conn.Query<T_Test>("Select Top 10 * From T_Test T Where 1=1  And 2=2 Order By Id Desc");
+                    var list = conn.Query<T_Test>("      SELECT Top 10 T.* From T_Test T With(NoLock)            Where 1=1      ");
                 }
             }
             Assert.Equal<int>(i, testTime);
