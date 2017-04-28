dbb941627b24f161eb7089c14384c8f682341322
Ahoo <tsgx_1989@qq.com>
2017-04-28 17:15:56 +0800

    u

---
+++ SmartSql.DataAccess/Abstractions/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartSql.DataAccess.Abstractions
+{
+    public interface IEntity<TPrimary>
+    {
+         TPrimary Id { get; set; }
+    }
+}
