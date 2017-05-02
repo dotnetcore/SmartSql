4f715d0d43e1b23b2522582265e219fde8741121
Ahoo <tsgx_1989@qq.com>
2017-05-02 17:01:37 +0800

    u

--- SmartSql/Exceptions/SmartSqlException.cs
+++ SmartSql/Exceptions/SmartSqlException.cs
@@ -9,7 +9,6 @@ namespace SmartSql.Exceptions
 		public SmartSqlException(): base("SmartSql throw an exception.") { }
         public SmartSqlException(Exception ex) : base("SmartSql throw an exception.", ex) { }
         public SmartSqlException(string message) : base ( message ) { }
-
         public SmartSqlException(string message, Exception inner) : base ( message, inner ) { }
 
     }
