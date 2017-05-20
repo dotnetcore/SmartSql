df9e2c17a333f74c7a21abb61230c3c94396c888
AhooWang <tsgx_1989@qq.com>
2017-05-21 00:05:24 +0800

    add ZooKeeperConfigLoader.cs

---
+++ SmartSql.ZooKeeperConfig/ZooKeeperConfigLoader.cs
@@ -0,0 +1,16 @@
+﻿using SmartSql.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartSql.SqlMap;
+
+namespace SmartSql.ZooKeeperConfig
+{
+    public class ZooKeeperConfigLoader : IConfigLoader
+    {
+        public SmartSqlMapConfig Load(string path, ISmartSqlMapper smartSqlMapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
