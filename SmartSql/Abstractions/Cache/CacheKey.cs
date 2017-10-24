deb41bc2dba4229731644463164fb934cc735d0a
Ahoo <ahoowang@qq.com>
2017-10-24 14:23:49 +0800

    1. fixed cache invalidation problem after used DynamicParameters for RequestContext.Request .

--- SmartSql/Abstractions/Cache/CacheKey.cs
+++ SmartSql/Abstractions/Cache/CacheKey.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Reflection;
 using System.Linq;
 using System.Collections;
+using Dapper;
 
 namespace SmartSql.Abstractions.Cache
 {
@@ -19,28 +20,47 @@ namespace SmartSql.Abstractions.Cache
             get
             {
                 if (RequestContext.Request == null) { return "Null"; }
-                var properties = RequestContext.Request.GetType().GetProperties().ToList().OrderBy(p => p.Name);
                 StringBuilder strBuilder = new StringBuilder();
-                foreach (var property in properties)
+                if (RequestContext.Request is DynamicParameters)
                 {
-                    var val = property.GetValue(RequestContext.Request);
-                    if (val is IEnumerable list && !(val is String))
+                    var reqParams = RequestContext.Request as DynamicParameters;
+                    var paramNames = reqParams.ParameterNames.ToList().OrderBy(p => p);
+                    foreach (var paramName in paramNames)
                     {
-                        strBuilder.Append("&(");
-                        foreach (var item in list)
-                        {
-                            strBuilder.AppendFormat("{0},", item);
-                        }
-                        strBuilder.Append(")");
+                        var val = reqParams.Get<object>(paramName);
+                        BuildSqlQueryString(strBuilder, paramName, val);
                     }
-                    else
+                }
+                else
+                {
+                    var properties = RequestContext.Request.GetType().GetProperties().ToList().OrderBy(p => p.Name);
+                    foreach (var property in properties)
                     {
-                        strBuilder.AppendFormat("&{0}", val);
+                        var val = property.GetValue(RequestContext.Request);
+                        BuildSqlQueryString(strBuilder, property.Name, val);
                     }
                 }
                 return strBuilder.ToString().Trim('&');
             }
         }
+
+        private void BuildSqlQueryString(StringBuilder strBuilder, string key, object val)
+        {
+            if (val is IEnumerable list && !(val is String))
+            {
+                strBuilder.AppendFormat("&{0}=(", key);
+                foreach (var item in list)
+                {
+                    strBuilder.AppendFormat("{0},", item);
+                }
+                strBuilder.Append(")");
+            }
+            else
+            {
+                strBuilder.AppendFormat("&{0}={1}", key, val);
+            }
+        }
+
         public String Key { get { return $"{RequestContext.FullSqlId}:{RequestQueryString}"; } }
         public CacheKey(RequestContext context)
         {
