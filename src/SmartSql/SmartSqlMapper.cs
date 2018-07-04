ab238078f714839bbf5f2f22e352ce83eded98db
Ahoo <ahoowang@qq.com>
2018-07-04 16:07:57 +0800

    1. add no typehandler errMsg       2. add default ISession.BeginSession

--- src/SmartSql/SmartSqlMapper.cs
+++ src/SmartSql/SmartSqlMapper.cs
@@ -346,17 +346,11 @@ namespace SmartSql
         #region Scoped Session
         public IDbConnectionSession BeginSession()
         {
-            if (SessionStore.LocalSession != null)
-            {
-                throw new SmartSqlException("SmartSqlMapper could not invoke BeginSession(). A LocalSession is already existed.");
-            }
             var reqContext = new RequestContext
             {
                 DataSourceChoice = DataSourceChoice.Read
             };
-            var dataSource = DataSourceFilter.Elect(reqContext);
-            var dbSession = SessionStore.CreateDbSession(dataSource);
-            dbSession.Begin();
+            var dbSession = BeginSession(reqContext);
             return dbSession;
         }
         public IDbConnectionSession BeginSession(RequestContext context)
