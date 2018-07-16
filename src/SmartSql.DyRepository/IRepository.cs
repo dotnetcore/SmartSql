5cc741ac5b6f6b80a29d0681ad0ca6a5dea071fa
Ahoo <ahoowang@qq.com>
2018-07-16 14:02:00 +0800

    1. add IRepositoryAsync

--- src/SmartSql.DyRepository/IRepository.cs
+++ src/SmartSql.DyRepository/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SmartSql.DyRepository
 {
@@ -30,5 +31,29 @@ namespace SmartSql.DyRepository
         TEntity GetById([Param("Id")]TPrimary id);
         [Statement(Execute = ExecuteBehavior.ExecuteScalar)]
         bool IsExist(object reqParams);
+    }
+    /// <summary>
+    /// 异步泛型仓储接口
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <typeparam name="TPrimary"></typeparam>
+    public interface IRepositoryAsync<TEntity, TPrimary> : IRepository
+    {
+        Task<int> InsertAsync(TEntity entity);
+        Task<int> DeleteAsync(object reqParams);
+        [Statement(Id = "Delete")]
+        Task<int> DeleteByIdAsync([Param("Id")]TPrimary id);
+        Task<int> UpdateAsync(TEntity entity);
+        [Statement(Id = "Update")]
+        Task<int> DyUpdateAsync(object dyObj);
+        Task<IEnumerable<TEntity>> QueryAsync(object reqParams);
+        Task<IEnumerable<TEntity>> QueryByPageAsync(object reqParams);
+        [Statement(Execute = ExecuteBehavior.ExecuteScalar)]
+        Task<int> GetRecordAsync(object reqParams);
+        Task<TEntity> GetEntityAsync(object reqParams);
+        [Statement(Id = "GetEntity")]
+        Task<TEntity> GetByIdAsync([Param("Id")]TPrimary id);
+        [Statement(Execute = ExecuteBehavior.ExecuteScalar)]
+        Task<bool> IsExistAsync(object reqParams);
     }
 }
