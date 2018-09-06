94358551821e865d833d73b0478628d2b944a940
Ahoo Wang <ahoowang@qq.com>
2018-09-07 00:33:42 +0800

    add PropertyValue

--- src/SmartSql/Utils/PropertyAccessor/IGetAccessorFactory.cs
+++ src/SmartSql/Utils/PropertyAccessor/IGetAccessorFactory.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace SmartSql.Utils.PropertyAccessor
 {
+    public class PropertyValue
+    {
+        public GetStatus Status { get; set; } = GetStatus.Ok;
+        public object Value { get; set; }
 
+        public enum GetStatus
+        {
+            Ok = 1,
+            NotFindProperty = 1 << 1,
+            NotFindGet = 1 << 2
+        }
+    }
     public interface IGetAccessorFactory
     {
         Func<object, object> CreateGet(Type targetType, string propertyName, bool ignorePropertyCase);
