e7b6484bb2ea90b4891f7e29d325a32a67b322cb
Ahoo Wang <ahoowang@qq.com>
2019-04-01 12:02:40 +0800

    add SmartSql.AOP add SmartSql.Extensions add XmlTypeHandler

---
+++ src/SmartSql.AOP/DbSessionAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspectCore.DynamicProxy;
+using SmartSql;
+namespace SmartSql.AOP
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class DbSessionAttribute : AbstractInterceptorAttribute
+    {
+        public string Alias { get; set; } = SmartSqlBuilder.DEFAULT_ALIAS;
+
+        public override Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            using (SmartSqlContainer.Instance.GetSmartSql(Alias).SmartSqlConfig.SessionStore.Open())
+            {
+                return next.Invoke(context);
+            }
+        }
+    }
+}
---
+++ src/SmartSql.AOP/TransactionAttribute.cs
@@ -0,0 +1,31 @@
+﻿using AspectCore.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartSql.AOP
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TransactionAttribute : AbstractInterceptorAttribute
+    {
+        public string Alias { get; set; } = SmartSqlBuilder.DEFAULT_ALIAS;
+        public IsolationLevel Level { get; set; } = IsolationLevel.Unspecified;
+        public override async Task Invoke(AspectContext context, AspectDelegate next)
+        {
+            var transaction = SmartSqlContainer.Instance.GetSmartSql(Alias).SqlMapper;
+            try
+            {
+                transaction.BeginTransaction();
+                await next.Invoke(context);
+                transaction.CommitTransaction();
+            }
+            catch (Exception ex)
+            {
+                transaction.RollbackTransaction();
+                throw ex;
+            }
+        }
+    }
+}
