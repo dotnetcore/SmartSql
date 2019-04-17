3a805767462d0bf9ec1070c531a4de3abc1c16a8
Ahoo Wang <ahoowang@qq.com>
2019-04-17 19:24:16 +0800

    support multiple IdGenerator

---
+++ src/SmartSql/IdGenerator/DbSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartSql.Configuration;
+
+namespace SmartSql.IdGenerator
+{
+    public class DbSequence : IIdGenerator
+    {
+        private SmartSqlConfig _smartSqlConfig;
+        
+        protected int Step { get; set; } = 1;
+        protected int DbId { get; set; } 
+        protected int CurrentId { get; set; } = 1;
+        protected String Sql { get; set; }
+
+        public void Initialize(IDictionary<string, object> parameters)
+        {
+            if (parameters.Value(nameof(DbSequence.Step), out int step))
+            {
+                Step = step;
+            }
+            parameters.EnsureValue(nameof(SmartSqlConfig), out _smartSqlConfig);
+        }
+
+        public long NextId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
