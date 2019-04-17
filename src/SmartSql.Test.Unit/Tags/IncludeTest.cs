043875c2ea50fe90d61fc50b3757aeec1c9864b0
rocher <rocher@126.com>
2019-04-17 19:35:20 +0800

    Add Env/Include Tag UnitTest

---
+++ src/SmartSql.Test.Unit/Tags/IncludeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SmartSql.Test.Unit.Tags
+{
+  public  class IncludeTest:AbstractXmlConfigBuilderTest
+    {
+        [Fact]
+        public void Include_Test()
+        {
+            var msg = DbSession.ExecuteScalar<String>(new RequestContext
+            {
+                Scope = nameof(IncludeTest),
+                SqlId = "Query",
+                Request = new { FLong = 1 }
+            });
+            Assert.True(true);
+        }
+    }
+}
