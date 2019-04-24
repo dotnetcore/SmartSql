1048cb3b42dad0790d70a7eff6d4f9d17cdb4e94
Ahoo Wang <ahoowang@qq.com>
2019-04-24 19:36:52 +0800

    add DefaultTypeTest.cs

---
+++ src/SmartSql.Test.Unit/Reflection/DefaultTypeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartSql.Reflection.TypeConstants;
+using Xunit;
+
+namespace SmartSql.Test.Unit.Reflection
+{
+    public class DefaultTypeTest
+    {
+        [Fact]
+        public void Default()
+        {
+           
+        }
+    }
+}
---
+++ src/SmartSql/Reflection/TypeConstants/DefaultType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SmartSql.Reflection.TypeConstants
+{
+    public static class DefaultType
+    {
+        public static readonly Type GenericType = typeof(Default<>);
+
+        public static Type MakeGenericType(Type type)
+        {
+            return GenericType.MakeGenericType(type);
+        }
+        public static FieldInfo GetDefaultField(Type type)
+        {
+            return MakeGenericType(type).GetField("Value");
+        }
+    }
+}
