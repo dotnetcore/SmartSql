ae2f6849f5debd6b9966d3368ca18a205c34a5a5
Ahoo Wang <ahoowang@qq.com>
2019-05-31 14:35:47 +0800

    add SmartSql.Cache.RabbitMQ proj

---
+++ src/SmartSql.Cache.RabbitMQ/CacheManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SmartSql.Cache.RabbitMQ
+{
+    public class CacheManager : ICacheManager
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetCache(ExecutionContext executionContext, out object cacheItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryAddCache(ExecutionContext executionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
