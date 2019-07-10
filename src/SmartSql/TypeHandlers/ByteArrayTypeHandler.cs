70f6f3803bfdca20e2acc884a8365e36fbdcbd94
Ahoo Wang <ahoowang@qq.com>
2019-07-10 11:51:11 +0800

    fix https://github.com/dotnetcore/SmartSql/issues/77

--- src/SmartSql/TypeHandlers/ByteArrayTypeHandler.cs
+++ src/SmartSql/TypeHandlers/ByteArrayTypeHandler.cs
@@ -6,24 +6,27 @@ using System.Text;
 
 namespace SmartSql.TypeHandlers
 {
-    public class ByteArrayTypeHandler : AbstractTypeHandler<Byte[], Byte[]>
+    public class ByteArrayTypeHandler : AbstractNullableTypeHandler<Byte[], Byte[]>
     {
-        public override byte[] GetValue(DataReaderWrapper dataReader, int columnIndex, Type targetType)
+        protected override byte[] GetValueWhenNotNull(DataReaderWrapper dataReader, int columnIndex)
         {
-            int bufferLength = (int)dataReader.GetBytes(columnIndex, 0, null, 0, 0);
+            var bufferLength = (int) dataReader.GetBytes(columnIndex, 0, null, 0, 0);
             byte[] byteArray = new byte[bufferLength];
+            if (bufferLength == 0)
+            {
+                return byteArray;
+            }
             dataReader.GetBytes(columnIndex, 0, byteArray, 0, bufferLength);
             return byteArray;
         }
     }
-    public class ByteArrayAnyTypeHandler : AbstractTypeHandler<Byte[], AnyFieldType>
+
+    public class ByteArrayAnyTypeHandler : AbstractNullableTypeHandler<Byte[], AnyFieldType>
     {
-        public override byte[] GetValue(DataReaderWrapper dataReader, int columnIndex, Type targetType)
+        protected override byte[] GetValueWhenNotNull(DataReaderWrapper dataReader, int columnIndex)
         {
-            int bufferLength = (int)dataReader.GetBytes(columnIndex, 0, null, 0, 0);
-            byte[] byteArray = new byte[bufferLength];
-            dataReader.GetBytes(columnIndex, 0, byteArray, 0, bufferLength);
-            return byteArray;
+            var fieldValue = dataReader.GetValue(columnIndex).ToString();
+            return Encoding.Default.GetBytes(fieldValue);
         }
     }
 }
