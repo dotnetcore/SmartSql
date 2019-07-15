6b3b96336557cfe4a922fb2b01fec9b4856919ae
Ahoo Wang <ahoowang@qq.com>
2019-07-15 15:09:37 +0800

    refactor FormatSql

--- src/SmartSql/Middlewares/PrepareStatementMiddleware.cs
+++ src/SmartSql/Middlewares/PrepareStatementMiddleware.cs
@@ -18,6 +18,7 @@ namespace SmartSql.Middlewares
         private readonly SqlParamAnalyzer _sqlParamAnalyzer;
         private readonly DbProviderFactory _dbProviderFactory;
         private readonly TypeHandlerFactory _typeHandlerFactory;
+
         public PrepareStatementMiddleware(SmartSqlConfig smartSqlConfig)
         {
             _logger = smartSqlConfig.LoggerFactory.CreateLogger<PrepareStatementMiddleware>();
@@ -25,48 +26,22 @@ namespace SmartSql.Middlewares
             _dbProviderFactory = smartSqlConfig.Database.DbProvider.Factory;
             _typeHandlerFactory = smartSqlConfig.TypeHandlerFactory;
         }
+
         public override void Invoke<TResult>(ExecutionContext executionContext)
         {
             InitParameters(executionContext);
             InvokeNext<TResult>(executionContext);
         }
+
         #region Init Parameter
+
         private void InitParameters(ExecutionContext executionContext)
         {
             BuildSql(executionContext.Request);
             BuildDbParameters(executionContext.Request);
             if (_logger.IsEnabled(LogLevel.Debug))
             {
-                var sourceParameters = executionContext.Request.Parameters.DbParameters.Values;
-                var cmdText = executionContext.Request.RealSql;
-                string dbParameterStr = string.Join(",", sourceParameters.Select(p => $"{p.ParameterName}={p.Value}"));
-                string realSql = _sqlParamAnalyzer.Replace(cmdText, (paramName, nameWithPrefix) =>
-                {
-                    var paramNameCompare = executionContext.SmartSqlConfig.Settings.IgnoreParameterCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture;
-                    var dbParam = sourceParameters.FirstOrDefault(m => String.Equals(m.ParameterName, paramName, paramNameCompare));
-                    if (dbParam == null) { return nameWithPrefix; }
-                    if (dbParam.Value == DBNull.Value) { return "NULL"; }
-                    switch (dbParam.DbType)
-                    {
-                        case DbType.AnsiString:
-                        case DbType.AnsiStringFixedLength:
-                        case DbType.DateTime:
-                        case DbType.DateTime2:
-                        case DbType.DateTimeOffset:
-                        case DbType.Guid:
-                        case DbType.String:
-                        case DbType.StringFixedLength:
-                        case DbType.Time:
-                        case DbType.Xml:
-                            { return $"'{dbParam.Value}'"; }
-                        case DbType.Boolean:
-                            {
-                                return ((bool)dbParam.Value) ? "1" : "0";
-                            }
-                    }
-                    return dbParam.Value.ToString();
-                });
-                _logger.LogDebug($"Statement.Id:[{executionContext.Request.FullSqlId}],Sql:{Environment.NewLine}{cmdText}{Environment.NewLine}Parameters:[{dbParameterStr}]{Environment.NewLine}Sql with parameter value: {Environment.NewLine}{realSql}");
+                _logger.LogDebug(executionContext.FormatSql(true));
             }
         }
 
@@ -96,16 +71,19 @@ namespace SmartSql.Middlewares
                         propertyName = parameter.Property;
                         typeHandler = parameter.Handler;
                     }
+
                     if (!reqConetxt.Parameters.TryGetValue(propertyName, out var sqlParameter))
                     {
                         continue;
                     }
+
                     var sourceParam = _dbProviderFactory.CreateParameter();
                     sourceParam.ParameterName = paramName;
 
                     if (typeHandler == null)
                     {
-                        typeHandler = sqlParameter.TypeHandler ?? _typeHandlerFactory.GetTypeHandler(sqlParameter.ParameterType);
+                        typeHandler = sqlParameter.TypeHandler ??
+                                      _typeHandlerFactory.GetTypeHandler(sqlParameter.ParameterType);
                     }
 
                     typeHandler.SetParameter(sourceParam, sqlParameter.Value);
@@ -121,18 +99,22 @@ namespace SmartSql.Middlewares
             {
                 sourceParam.DbType = sqlParameter.DbType.Value;
             }
+
             if (sqlParameter.Direction.HasValue)
             {
                 sourceParam.Direction = sqlParameter.Direction.Value;
             }
+
             if (sqlParameter.Precision.HasValue)
             {
                 sourceParam.Precision = sqlParameter.Precision.Value;
             }
+
             if (sqlParameter.Scale.HasValue)
             {
                 sourceParam.Scale = sqlParameter.Scale.Value;
             }
+
             if (sqlParameter.Size.HasValue)
             {
                 sourceParam.Size = sqlParameter.Size.Value;
@@ -145,11 +127,14 @@ namespace SmartSql.Middlewares
             {
                 return;
             }
+
             requestContext.SqlBuilder = new StringBuilder();
             requestContext.Statement.BuildSql(requestContext);
             requestContext.RealSql = requestContext.SqlBuilder.ToString().Trim();
         }
+
         #endregion
+
         public override async Task InvokeAsync<TResult>(ExecutionContext executionContext)
         {
             InitParameters(executionContext);
