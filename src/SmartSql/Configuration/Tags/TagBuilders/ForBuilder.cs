f24abbc70391e85cc0be8ddbd14fdad4f85f84d0
Ahoo Wang <ahoowang@qq.com>
2019-07-25 16:20:04 +0800

    add support [For-tag] nest-value-access optimize Cache

--- src/SmartSql/Configuration/Tags/TagBuilders/ForBuilder.cs
+++ src/SmartSql/Configuration/Tags/TagBuilders/ForBuilder.cs
@@ -9,15 +9,16 @@ namespace SmartSql.Configuration.Tags.TagBuilders
     {
         public override ITag Build(XmlNode xmlNode, Statement statement)
         {
+            var property = GetProperty(xmlNode);
             return new For
             {
-                Property = GetProperty(xmlNode),
+                Property = property,
                 Prepend = GetPrepend(xmlNode),
                 Required = GetRequired(xmlNode),
                 Open = GetXmlAttributeValue(xmlNode, nameof(For.Open)),
                 Close = GetXmlAttributeValue(xmlNode, nameof(For.Close)),
                 Separator = GetXmlAttributeValue(xmlNode, nameof(For.Separator)),
-                Key = GetXmlAttributeValue(xmlNode, nameof(For.Key)),
+                Key = GetXmlAttributeValue(xmlNode, nameof(For.Key)) ?? property,
                 ChildTags = new List<ITag>(),
                 Statement = statement
             };
