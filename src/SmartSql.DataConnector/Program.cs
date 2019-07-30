7d375e03470d54237cb1957f47d7b2ad14e12a65
Ahoo Wang <ahoowang@qq.com>
2019-07-31 00:18:44 +0800

    add SmartSql.DataConnector proj

---
+++ src/SmartSql.DataConnector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmartSql.DataConnector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SmartSql.DataConnector!");
+        }
+    }
+}
