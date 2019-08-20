a30a6116fc5ebae3c11226f0cf0f8b987974c0f4
Ahoo Wang <ahoowang@qq.com>
2019-08-20 17:44:54 +0800

    fix ISqlMapperExtensions SessionStore.Dispose bug

--- src/SmartSql/CUD/ISqlMapperExtensions.cs
+++ src/SmartSql/CUD/ISqlMapperExtensions.cs
@@ -7,60 +7,84 @@ namespace SmartSql
 {
     public static class ISqlMapperExtensions
     {
+        internal static TResult ExecuteImpl<TResult>(this ISqlMapper sqlMapper, Func<IDbSession, TResult> executeFunc)
+        {
+            //Session 释放原则：谁开启，谁释放
+            var dbSession = sqlMapper.SessionStore.LocalSession;
+            var ownSession = dbSession == null;
+            try
+            {
+                if (ownSession)
+                {
+                    dbSession = sqlMapper.SessionStore.Open();
+                }
+
+                return executeFunc(dbSession);
+            }
+            finally
+            {
+                if (ownSession)
+                {
+                    sqlMapper.SessionStore.Dispose();
+                }
+            }
+        }
+
         public static int DeleteAll<TEntity>(this ISqlMapper sqlMapper)
         {
-            return sqlMapper.SessionStore.Open().DeleteAll<TEntity>();
+            return ExecuteImpl(sqlMapper, session => session.DeleteAll<TEntity>());
         }
 
         public static int DeleteById<TEntity, TPrimaryKey>(this ISqlMapper sqlMapper, TPrimaryKey id)
         {
-            return sqlMapper.SessionStore.Open().DeleteById<TEntity, TPrimaryKey>(id);
+            return ExecuteImpl(sqlMapper, session => session.DeleteById<TEntity, TPrimaryKey>(id));
         }
 
         public static int DeleteMany<TEntity, TPrimaryKey>(this ISqlMapper sqlMapper, IEnumerable<TPrimaryKey> ids)
         {
-            return sqlMapper.SessionStore.Open().DeleteMany<TEntity, TPrimaryKey>(ids);
+            return ExecuteImpl(sqlMapper, session => session.DeleteMany<TEntity, TPrimaryKey>(ids));
         }
 
         public static TEntity GetById<TEntity, TPrimaryKey>(this ISqlMapper sqlMapper, TPrimaryKey id)
         {
-            return GetById<TEntity, TPrimaryKey>(sqlMapper, id, false);
+            return ExecuteImpl(sqlMapper, session => session.GetById<TEntity, TPrimaryKey>(id));
         }
 
         public static TEntity GetById<TEntity, TPrimaryKey>(this ISqlMapper sqlMapper, TPrimaryKey id,
             bool enablePropertyChangedTrack)
         {
-            return sqlMapper.SessionStore.Open().GetById<TEntity, TPrimaryKey>(id, enablePropertyChangedTrack);
+            return ExecuteImpl(sqlMapper,
+                session => session.GetById<TEntity, TPrimaryKey>(id, enablePropertyChangedTrack));
         }
 
         public static TPrimaryKey Insert<TEntity, TPrimaryKey>(this ISqlMapper sqlMapper, TEntity entity)
         {
-            return sqlMapper.SessionStore.Open().Insert<TEntity, TPrimaryKey>(entity);
+            return ExecuteImpl(sqlMapper, session => session.Insert<TEntity, TPrimaryKey>(entity));
         }
 
         public static int Insert<TEntity>(this ISqlMapper sqlMapper, TEntity entity)
         {
-            return sqlMapper.SessionStore.Open().Insert<TEntity>(entity);
+            return ExecuteImpl(sqlMapper, session => session.Insert(entity));
         }
 
         public static int Update<TEntity>(this ISqlMapper sqlMapper, TEntity entity)
         {
-            return sqlMapper.SessionStore.Open().Update<TEntity>(entity);
+            return ExecuteImpl(sqlMapper, session => session.Update(entity));
         }
 
         public static int Update<TEntity>(this ISqlMapper sqlMapper, TEntity entity, bool enablePropertyChangedTrack)
         {
-            return sqlMapper.SessionStore.Open().Update<TEntity>(entity, enablePropertyChangedTrack);
+            return ExecuteImpl(sqlMapper, session => session.Update(entity, enablePropertyChangedTrack));
         }
 
         public static int DyUpdate<TEntity>(this ISqlMapper sqlMapper, object entity)
         {
-            return sqlMapper.SessionStore.Open().DyUpdate<TEntity>(entity);
+            return ExecuteImpl(sqlMapper, session => session.DyUpdate<TEntity>(entity));
         }
 
         public static int DyUpdate<TEntity>(this ISqlMapper sqlMapper, object entity, bool? enablePropertyChangedTrack)
         {
-            return sqlMapper.SessionStore.Open().DyUpdate<TEntity>(entity, enablePropertyChangedTrack);
+            return ExecuteImpl(sqlMapper, session => session.DyUpdate<TEntity>(entity, enablePropertyChangedTrack));
         }
     }
 }
