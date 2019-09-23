a3d586fc354474c70dc4968832335351507ccd49
Ahoo Wang <ahoowang@qq.com>
2019-09-23 19:12:06 +0800

    optimize ResolveProperty

--- src/SmartSql/Deserializer/EntityDeserializer.cs
+++ src/SmartSql/Deserializer/EntityDeserializer.cs
@@ -9,6 +9,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using SmartSql.Annotations;
 using SmartSql.Configuration;
 using SmartSql.CUD;
 using SmartSql.Reflection;
@@ -110,19 +111,19 @@ namespace SmartSql.Deserializer
 
             var dataReader = executionContext.DataReaderWrapper;
             var resultMap = executionContext.Request.GetCurrentResultMap();
-            
+
             var constructorMap = resultMap?.Constructor;
             var columns = Enumerable.Range(0, dataReader.FieldCount)
-                                    .Select(i => new
-                                    {
-                                        Index = i,
-                                        Name = dataReader.GetName(i),
-                                        ConvertedName = executionContext.Request.AutoConverter == null
-                                                   ? dataReader.GetName(i)
-                                                   : executionContext.Request.AutoConverter.Convert(dataReader.GetName(i)),
-                                        FieldType = dataReader.GetFieldType(i)
-                                    })
-                                    .ToDictionary(col => col.Name);
+                .Select(i => new ColumnDescriptor
+                {
+                    Index = i,
+                    ColumnName = dataReader.GetName(i),
+                    PropertyName = executionContext.Request.AutoConverter == null
+                        ? dataReader.GetName(i)
+                        : executionContext.Request.AutoConverter.Convert(dataReader.GetName(i)),
+                    FieldType = dataReader.GetFieldType(i)
+                })
+                .ToDictionary(col => col.ColumnName);
 
             var deserFunc = new DynamicMethod("Deserialize" + Guid.NewGuid().ToString("N"), resultType,
                 new[] {DataType.DataReaderWrapper, RequestContextType.AbstractType}, resultType, true);
@@ -163,53 +164,27 @@ namespace SmartSql.Deserializer
             ilGen.StoreLocalVar(0);
             foreach (var col in columns)
             {
-                var colName = col.Key;
-                var colIndex = col.Value.Index;
-                var filedType = col.Value.FieldType;
-                PropertyInfo propertyInfo = null;
-                string typeHandler = null;
-                var isDbNullLabel = ilGen.DefineLabel();
+                var columnDescriptor = col.Value;
+                var colIndex = columnDescriptor.Index;
 
                 #region Ensure Property & TypeHanlder
 
-                if (resultMap?.Properties != null)
-                {
-                    var propertyName = colName;
-                    if (resultMap.Properties.TryGetValue(colName, out var resultProperty))
-                    {
-                        propertyName = resultProperty.Name;
-                        typeHandler = resultProperty.TypeHandler;
-                    }
-
-                    propertyInfo = resultType.GetProperty(propertyName);
-                }
-                else
-                {
-                    colName = col.Value.ConvertedName;
-                }
-
-                if (EntityMetaDataCache<TResult>.TryGetColumnByColumnName(colName, out var columnAttribute))
-                {
-                    propertyInfo = columnAttribute.Property;
-                    if (!String.IsNullOrEmpty(columnAttribute.TypeHandler))
-                    {
-                        typeHandler = columnAttribute.TypeHandler;
-                    }
-                }
-
-                if (propertyInfo == null)
+                if (!ResolveProperty<TResult>(resultMap, resultType, col.Value, out var propertyHolder)
+                )
                 {
                     continue;
                 }
 
-                if (!propertyInfo.CanWrite)
+                if (!propertyHolder.CanWrite)
                 {
                     continue;
                 }
 
                 #endregion
 
-                var propertyType = propertyInfo.PropertyType;
+                var isDbNullLabel = ilGen.DefineLabel();
+
+                var propertyType = propertyHolder.PropertyType;
                 if (ignoreDbNull)
                 {
                     ilGen.LoadArg(0);
@@ -219,8 +194,9 @@ namespace SmartSql.Deserializer
                 }
 
                 ilGen.LoadLocalVar(0);
-                LoadPropertyValue(ilGen, executionContext, colIndex, propertyType, filedType, typeHandler);
-                ilGen.Call(propertyInfo.SetMethod);
+                LoadPropertyValue(ilGen, executionContext, colIndex, propertyType, columnDescriptor.FieldType,
+                    propertyHolder.TypeHandler);
+                ilGen.Call(propertyHolder.SetMethod);
                 if (ignoreDbNull)
                 {
                     ilGen.MarkLabel(isDbNullLabel);
@@ -239,6 +215,48 @@ namespace SmartSql.Deserializer
             ilGen.LoadLocalVar(0);
             ilGen.Return();
             return deserFunc.CreateDelegate(typeof(Func<DataReaderWrapper, AbstractRequestContext, TResult>));
+        }
+
+        private static bool ResolveProperty<TResult>(ResultMap resultMap, Type resultType,
+            ColumnDescriptor columnDescriptor
+            , out PropertyHolder propertyHolder)
+        {
+            propertyHolder = null;
+            if (resultMap?.Properties != null)
+            {
+                if (resultMap.Properties.TryGetValue(columnDescriptor.ColumnName, out var resultProperty))
+                {
+                    propertyHolder = new PropertyHolder
+                    {
+                        Property = resultType.GetProperty(resultProperty.Name),
+                        TypeHandler = resultProperty.TypeHandler
+                    };
+                    return true;
+                }
+            }
+
+            if (EntityMetaDataCache<TResult>.TryGetColumnByColumnName(columnDescriptor.ColumnName, out var columnAttribute))
+            {
+                propertyHolder = new PropertyHolder
+                {
+                    Property = columnAttribute.Property,
+                    TypeHandler = columnAttribute.TypeHandler
+                };
+                return true;
+            }
+
+            if (EntityMetaDataCache<TResult>.TryGetColumnByPropertyName(columnDescriptor.PropertyName,
+                out columnAttribute))
+            {
+                propertyHolder = new PropertyHolder
+                {
+                    Property = columnAttribute.Property,
+                    TypeHandler = columnAttribute.TypeHandler
+                };
+                return true;
+            }
+
+            return false;
         }
 
         private void LoadPropertyValue(ILGenerator ilGen, ExecutionContext executionContext, int colIndex,
@@ -309,6 +327,15 @@ namespace SmartSql.Deserializer
         public void SetupSmartSql(SmartSqlBuilder smartSqlBuilder)
         {
             _logger = smartSqlBuilder.SmartSqlConfig.LoggerFactory.CreateLogger<EntityDeserializer>();
+        }
+
+
+        public class ColumnDescriptor
+        {
+            public int Index { get; set; }
+            public String ColumnName { get; set; }
+            public Type FieldType { get; set; }
+            public String PropertyName { get; set; }
         }
     }
 }
