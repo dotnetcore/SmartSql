fdd2ca28ca5507800b606aa1c5fb4ec4cd42a2a9
yemobai <546307885@qq.com>
2020-11-04 23:56:06 +0800

    解决DataBase只有一个Read节点时，而且没有写weight属性，导致权重选择出异常的问题，同时在build xmlConfig时，强制验证Read节点是否写了Weight属性，当Read节点大于2时，其中一个read节点没有写weight属性，直接抛异常

--- src/SmartSql.Test.Unit/ConfigBuilder/XmlConfigLoaderTest.cs
+++ src/SmartSql.Test.Unit/ConfigBuilder/XmlConfigLoaderTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using SmartSql.ConfigBuilder;
+using SmartSql.Exceptions;
 using Xunit;
 
 namespace SmartSql.Test.Unit.ConfigBuilder
@@ -13,6 +14,19 @@ namespace SmartSql.Test.Unit.ConfigBuilder
         {
             var configLoader = new XmlConfigBuilder(ResourceType.File, "SmartSqlMapConfig.xml");
             var config = configLoader.Build();
+        }
+        [Fact]
+        public void LoadFailTest()
+        {
+            try
+            {
+                var configLoader = new XmlConfigBuilder(ResourceType.File, "SmartSqlMapConfig.xml");
+                var config = configLoader.Build();
+            }
+            catch (SmartSqlException ex)
+            {                  
+                Assert.StartsWith( "Read Nodes must have Weight attribute",ex.Message);
+            }
         }
     }
 }
