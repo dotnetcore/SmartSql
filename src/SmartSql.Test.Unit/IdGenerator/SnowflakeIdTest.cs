f29de91104759e1eb248c68b6fb91ab5aa657f00
Ahoo Wang <ahoowang@qq.com>
2022-04-12 14:21:51 +0800

    refactor SnowflakeIdTest

--- src/SmartSql.Test.Unit/IdGenerator/SnowflakeIdTest.cs
+++ src/SmartSql.Test.Unit/IdGenerator/SnowflakeIdTest.cs
@@ -7,40 +7,16 @@ using Xunit;
 
 namespace SmartSql.Test.Unit.IdGenerator
 {
-    [Collection("GlobalSmartSql")]
     public class SnowflakeIdTest
     {
-        protected ISqlMapper SqlMapper { get; }
-
-        public SnowflakeIdTest(SmartSqlFixture smartSqlFixture)
-        {
-            SqlMapper = smartSqlFixture.SqlMapper;
-        }
-
-        // TODO
-        [Fact(Skip = "TODO")]
+        [Fact]
         public void NextId()
         {
             var id = SnowflakeId.Default.NextId();
+            Assert.NotEqual(0, id);
             var idState = SnowflakeId.Default.FromId(id);
-            
-            Assert.NotEqual(0, id);
-        }
-
-        // TODO
-        [Fact(Skip = "TODO")]
-        public void Insert()
-        {
-            var id = SqlMapper.ExecuteScalar<long>(new RequestContext
-            {
-                Scope = nameof(UseIdGenEntity),
-                SqlId = "Insert",
-                Request = new UseIdGenEntity()
-                {
-                    Name = "SmartSql"
-                }
-            });
-            Assert.NotEqual(0, id);
+            Assert.Equal(id, idState.Id);
+            Assert.Equal(DateTime.UtcNow.Date, idState.UtcTime.Date);
         }
     }
 }
