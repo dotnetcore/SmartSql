5ce6a4b5734d74a79467ffd877e8133d3b9d7a69
Ahoo Wang <ahoowang@qq.com>
2022-04-12 12:15:48 +0800

    refactor SetTest

--- src/SmartSql.Test.Unit/Tags/SetTest.cs
+++ src/SmartSql.Test.Unit/Tags/SetTest.cs
@@ -2,31 +2,57 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SmartSql.Configuration;
+using SmartSql.Configuration.Tags;
 using Xunit;
 
 namespace SmartSql.Test.Unit.Tags
 {
     [Collection("GlobalSmartSql")]
-    public class SetTest 
+    public class SetTest
     {
-        protected ISqlMapper SqlMapper { get; }
+        SmartSqlConfig SmartSqlConfig { get; }
 
         public SetTest(SmartSqlFixture smartSqlFixture)
         {
-            SqlMapper = smartSqlFixture.SqlMapper;
-        }
-        // TODO
-        [Fact(Skip = "TODO")]
-        public void Set_Test()
-        {
-            var iRows = SqlMapper.Execute(new RequestContext
-            {
-                Scope = nameof(SetTest),
-                SqlId = "UpdateUser",
-                Request = new { UserName = "MySmartSql",Status=1,Id=1 }
-            });
-            Assert.True(true);
+            SmartSqlConfig = smartSqlFixture.SqlMapper.SmartSqlConfig;
         }
 
+        [Fact]
+        public void Set()
+        {
+            var requestCtx = new RequestContext
+            {
+                Scope = "TagTest",
+                SqlId = nameof(Set),
+                Request = new { Property1 = "SmartSql", Property2 = 1, Id = 1 }
+            };
+            requestCtx.SetupParameters();
+
+            var statement = SmartSqlConfig.GetStatement(requestCtx.FullSqlId);
+            statement.BuildSql(requestCtx);
+
+            Assert.Equal(@"Update T_Table
+             Set   
+                    Property1=@Property1
+                 , 
+                    Property2=@Property2
+                
+            Where Id=@Id", requestCtx.SqlBuilder.ToString().Trim());
+        }
+
+        [Fact]
+        public void SetWhenRequestIsEmpty()
+        {
+            var requestCtx = new RequestContext
+            {
+                Scope = "TagTest",
+                SqlId = nameof(Set)
+            };
+            requestCtx.SetupParameters();
+
+            var statement = SmartSqlConfig.GetStatement(requestCtx.FullSqlId);
+            Assert.Throws<TagMinMatchedFailException>(() => { statement.BuildSql(requestCtx); });
+        }
     }
 }
