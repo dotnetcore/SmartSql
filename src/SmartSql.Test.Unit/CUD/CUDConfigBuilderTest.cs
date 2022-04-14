1af77df82bbb0b094a2b16585464c614a1604f9a
Gmij <gmij@qq.com>
2022-04-14 14:21:03 +0800

    完善单元测试

--- src/SmartSql.Test.Unit/CUD/CUDConfigBuilderTest.cs
+++ src/SmartSql.Test.Unit/CUD/CUDConfigBuilderTest.cs
@@ -12,24 +12,28 @@ namespace SmartSql.Test.Unit.CUD
         [Fact]
         public void Build()
         {
+            var xmlConfigBuilder =
+                new XmlConfigBuilder(ResourceType.File, SmartSqlBuilder.DEFAULT_SMARTSQL_CONFIG_PATH);
             var entityTypeList = TypeScan.Scan(new TypeScanOptions
             {
                 AssemblyString = "SmartSql.Test",
-                Filter = type => type.Namespace == "SmartSql.Test.Entities"
+                Filter = type => type.FullName == "SmartSql.Test.Entities.WebMenu"
             });
-            var configBuilder = new CUDConfigBuilder(entityTypeList);
+            var configBuilder = new CUDConfigBuilder(xmlConfigBuilder,entityTypeList);
             var smartSqlConfig = configBuilder.Build();
             Assert.NotNull(smartSqlConfig);
             var maps = smartSqlConfig.SqlMaps;
             foreach (var map in maps)
             {
+                if (map.Key != "WebMenu")
+                    continue;
                 var scope = map.Key;
                 var v = map.Value;
                 foreach (var statement in v.Statements)
                 {
                     Console.WriteLine($"class {scope}.{statement.Value.Id} found");
                 }
-                Assert.Equal(5, v.Statements.Count);
+                Assert.Equal(7, v.Statements.Count);
             }
         }
 
