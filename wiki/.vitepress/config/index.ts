c3029aff449a91d6631c8950d4abb865d5d86df1
Ahoo Wang <ahoowang@qq.com>
2026-05-12 21:28:20 +0800

    feat: add Google Analytics (G-ZFQNQXMXS3) to wiki

--- wiki/.vitepress/config/index.ts
+++ wiki/.vitepress/config/index.ts
@@ -12,6 +12,8 @@ export default defineConfig({
   ],
   head: [
     ['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' }],
+    ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-ZFQNQXMXS3' }],
+    ['script', {}, `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-ZFQNQXMXS3');`],
   ],
   locales: {
     root: {
